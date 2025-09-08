4c3c3caa280e631ece511a8ff11dd2d9d4384a44
nhh95 <namhh0101001@gmail.com>
2025-09-09 03:30:48 +0900

    -minio 디버깅

--- src/main/java/com/blog/myblog/domain/file/MinioService.java
+++ src/main/java/com/blog/myblog/domain/file/MinioService.java
@@ -30,11 +30,25 @@ public class MinioService {
 
     //minio에 이미지 전송
     public String minioUploadFile(File file) {
+
+        System.out.println("=== MinIO 업로드 시작 ===");
+        System.out.println("파일명: " + file.getName());
+        System.out.println("파일 크기: " + file.length());
+        System.out.println("파일 존재 여부: " + file.exists());
+        System.out.println("파일 읽기 가능: " + file.canRead());
+        System.out.println("버킷명: " + bucketName);
+        System.out.println("MinIO URL: " + minioUrl);
+
+
         try (InputStream inputStream = new FileInputStream(file)) {
 
             // 파일명 중복 방지를 위해 UUID와 원본 파일명 조합
             String minioFileName = UUID.randomUUID().toString() + "_" + file.getName();
             String minioObjectPath = "summernoteImage/" + minioFileName;
+
+            System.out.println("MinIO 파일명: " + minioFileName);
+            System.out.println("MinIO 객체 경로: " + minioObjectPath);
+
             // MinIO 버킷에 파일 업로드
             minioClient.putObject(
                     PutObjectArgs.builder()
@@ -45,10 +59,18 @@ public class MinioService {
                             .build()
             );
 
-            return minioUrl + "/" + bucketName + "/summernoteImage/" + minioFileName;
+            String resultUrl = minioUrl + "/" + bucketName + "/summernoteImage/" + minioFileName;
+            System.out.println("업로드 성공 결과 URL: " + resultUrl);
+            return resultUrl;
 
         } catch (Exception e) {
+            System.err.println("MinIO 업로드 실패");
+            System.err.println("에러 타입: " + e.getClass().getSimpleName());
+            System.err.println("에러 메시지: " + e.getMessage());
+            e.printStackTrace();
+
             throw new RuntimeException("MinIO UploadFile failed", e);
+
         }
     }
 
